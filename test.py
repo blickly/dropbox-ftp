acc7f64a98c13e68505e1146b6a9486df77b6943
Ben Lickly <blickly@eecs.berkeley.edu>
2012-04-17 10:48:09 -0700

    Fix revision number

--- test.py
+++ test.py
@@ -2,11 +2,11 @@
 from dropbox import client, rest, session
 
 # Get your app key and secret from the Dropbox developer website
-APP_KEY = 'INSERT_APP_KEY_HERE'
-APP_SECRET = 'INSERT_SECRET_HERE'
+APP_KEY = 'dypmsdyctp1jiv4'
+APP_SECRET = 'ihiakn2tr86h48e'
 
 # ACCESS_TYPE should be 'dropbox' or 'app_folder' as configured for your app
-ACCESS_TYPE = 'INSERT_ACCESS_TYPE_HERE'
+ACCESS_TYPE = 'app_folder'
 
 sess = session.DropboxSession(APP_KEY, APP_SECRET, ACCESS_TYPE)
 
@@ -16,12 +16,28 @@ url = sess.build_authorize_url(request_token)
 
 # Make the user sign in and authorize this token
 print "url:", url
+import os
+os.system('open "'+url+'"')
 print "Please visit this website and press the 'Allow' button, then hit 'Enter' here."
 raw_input()
 
 # This will fail if the user didn't visit the above URL and hit 'Allow'
 access_token = sess.obtain_access_token(request_token)
 
+print "dir(client):", dir(client)
+
 client = client.DropboxClient(sess)
 print "linked account:", client.account_info()
+
+f = open('test.py')
+response = client.put_file('/magnum-opus.py', f)
+print "uploaded:", response
+
+folder_metadata = client.metadata('/')
+print "metadata:", folder_metadata
+
+f, metadata = client.get_file_and_metadata('/magnum-opus.py',rev='1')
+out = open('magnum-opus.txt', 'w')
+out.write(f)
+print(metadata)
 
